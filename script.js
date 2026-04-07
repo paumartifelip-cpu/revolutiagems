5e29a43da5699ee6ffbc36d3905383f4bffa3358
paumartifelip <paumartifelip@gmail.com>
2026-04-07 13:07:56 -0600

    Subida inicial: diseño minimalista y enlaces de GEMS

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.gem-card').forEach(card => {
+    card.addEventListener('mousemove', e => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        card.style.setProperty('--mouse-x', `${x}px`);
+        card.style.setProperty('--mouse-y', `${y}px`);
+    });
+});
